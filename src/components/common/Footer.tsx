4add1a2d43535f06f799a4fb2c7f7c17268b6451
Rian Smart <rismart1221@outlook.com>
2025-06-14 23:15:43 -0700

    Update Footer component; change text color to ensure visibility in dark mode while maintaining visual consistency across the application.

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
     ">
       <div className="flex flex-col items-center gap-2">
         <p className="text-center text-base font-medium text-black dark:text-white">
-          <span className="hidden md:inline text-md font-semibold bg-gradient-to-r from-indigo-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">&copy; {new Date().getFullYear()} Rian Smart</span>. All rights reserved.
+          <span className="hidden md:inline text-md font-semibold text-black dark:text-white">&copy; {new Date().getFullYear()} Rian Smart</span>. All rights reserved.
         </p>
       </div>
     </footer>
